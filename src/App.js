9682969658cb9d70628268738f74e08bcae0ea30
Muhammad Umer <mumer271992@gmail.com>
2020-02-04 22:41:26 +0500

    Basic route configured for users list page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import './App.scss';
+import List from './pages/Users/List/List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/users/list" />
+          </Route>
+          <Route path="/users/list" component={List} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Users/List/List.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const List = () => {
+  return <div className="users-list-page">This is users list page</div>;
+};
+
+export default List;
