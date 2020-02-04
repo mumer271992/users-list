3f24f9e77d62c8873d3ffc61817525b3546ae106
Muhammad Umer <mumer271992@gmail.com>
2020-02-05 01:18:32 +0500

    Displays first page on users list page

--- src/pages/Users/List/List.js
+++ src/pages/Users/List/List.js
@@ -1,15 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
+import UsersList from '../../../components/UsersList/UsersList';
+
 const List = ({ list, fetchUsers }) => {
+  const initialState = {
+    page: 0,
+    pageSize: 10,
+    users: []
+  };
+  const [state, setState] = useState(initialState);
+
+  const onNextPage = useCallback(() => {
+    const start = state.pageSize * state.page;
+    const end = start + state.pageSize;
+
+    setState({
+      ...initialState,
+      page: state.page + 1,
+      users: list.slice(start, end)
+    });
+  }, [list, state]);
+
   useEffect(() => {
     if (!list || !list.length) {
-      console.log('Fetching List of users');
       fetchUsers();
+    }
+    // Populate first page
+    if (list && list.length) {
+      onNextPage();
     }
   }, [list, fetchUsers]);
 
-  return <div className="users-list-page">This is users list page</div>;
+  return (
+    <div className="users-list-page">
+      <UsersList list={state.users} />
+      <button onClick={() => onNextPage()}>Load Next</button>
+    </div>
+  );
 };
 
 List.propTypes = {
